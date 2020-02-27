5e524aa250369ec1d2d6dbc19a1bbab29ddb3d6a
Drew Tyre <atyre2@unl.edu>
2020-02-27 14:39:17 -0600

    return results as a list and make predictions only from AIC best model

--- R/WNV_models_lags.r
+++ R/WNV_models_lags.r
@@ -2,6 +2,12 @@
 models_lags = function(models, allLagsT, allLagsO, results.path){
   
   allfits <- map(models, ~gam(as.formula(.x), data=allLagsT, family=nb()))
-  preds <- predict_wYr(allfits[[1]], allLagsT, allLagsO)
-  return(preds)
+  # only predict from AIC best model
+  AICfits <- map_dbl(allfits, MuMIn::AICc)
+  best <- which.min(AICfits)
+  preds <- predict_wYr(allfits[[best]], allLagsT, allLagsO)
+  return(list(predictions= preds,
+              other = list(fittedModels=allfits,
+                           AICfits = AICfits,
+                           best = best)))
 }
