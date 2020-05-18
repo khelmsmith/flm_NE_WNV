66e8b67200211828650c94a74b8c19d6ffb7e304
Drew Tyre <atyre2@unl.edu>
2020-05-18 16:20:17 -0500

    put arthur county fix back

--- data-raw/sampledat.R
+++ data-raw/sampledat.R
@@ -8,8 +8,8 @@ sampledat <- readr::read_csv("data-raw/predictionsthrough2018.csv") %>%
 # given a 1, 1 gamma prior we have gamma(1, 17) as the posterior. This
 # still has a probability 0.52 of observing 16 zeros at the median. 
 # So the median is 0.04077336
-# sampledat <- dplyr::bind_rows(sampledat, dplyr::tibble(year = 2002:2019,
-#                              County = "Arthur",
-#                              pop100K = 0.00445,
-#                              cases = 0.0477336))
+sampledat <- dplyr::bind_rows(sampledat, dplyr::tibble(year = 2002:2019,
+                             County = "Arthur",
+                             pop100K = 0.00445,
+                             cases = 0.0477336))
 usethis::use_data(sampledat, overwrite = TRUE)
