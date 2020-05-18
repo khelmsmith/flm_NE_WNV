697c50a38b0183086bae28f6b3ef88c95e206ca0
Drew Tyre <atyre2@unl.edu>
2020-05-18 15:30:48 -0500

    Just leave out arthur county

--- data-raw/sampledat.R
+++ data-raw/sampledat.R
@@ -2,13 +2,14 @@
 # make sampledat from fitted model
 # assumes an input file with the fitted values in the cases column.
 #sampledat <- readr::read_csv("data-raw/sampledat.csv")
-sampledat <- readr::read_csv("data-raw/predictionsthrough2018.csv")
+sampledat <- readr::read_csv("data-raw/predictionsthrough2018.csv") %>% 
+  rename(cases = pred)
 # fix arthur county with bayesian posterior for 16 samples of 0 from a poisson distribution
 # given a 1, 1 gamma prior we have gamma(1, 17) as the posterior. This
 # still has a probability 0.52 of observing 16 zeros at the median. 
 # So the median is 0.04077336
-sampledat <- dplyr::bind_rows(sampledat, dplyr::tibble(year = 2002:2019,
-                             County = "Arthur",
-                             pop100K = 0.00445,
-                             cases = 0.0477336))
+# sampledat <- dplyr::bind_rows(sampledat, dplyr::tibble(year = 2002:2019,
+#                              County = "Arthur",
+#                              pop100K = 0.00445,
+#                              cases = 0.0477336))
 usethis::use_data(sampledat, overwrite = TRUE)
